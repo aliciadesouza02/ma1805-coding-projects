03580ef79018259a5a8721230dd76fe343b9c6d3
aliciadesouza02 <aliciadesouza02@gmail.com>
2025-10-21 13:44:01 +0100

    Add files via upload

---
+++ 01.Drawing/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle (200,200,50);
+  textSize(20);
+  text ("hello world", 200,200);
+
+}
