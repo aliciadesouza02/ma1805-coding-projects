99fb9c7e3f63da1b8bb600d3f5ab12fe7ba8f78b
aliciadesouza02 <aliciadesouza02@gmail.com>
2025-11-03 16:48:42 +0000

    Add files via upload

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -1,11 +1,62 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
 
+function setup() {
+  createCanvas(700, 700);
+}
 function draw() {
   background(220);
-  circle (200,200,50);
-  textSize(20);
-  text ("hello world", 200,200);
 
+
+  //white background
+  background(255);
+
+  //turn off default black outline on shapes
+  noStroke();
+
+  //black circle - base for Mickey's face
+  fill(0, 0, 0);
+  circle(200, 200, 200);
+
+  //smaller black circles for Mickey's ears
+  fill(0, 0, 0);
+  circle(100, 100, 100);
+
+  fill(0, 0, 0);
+  circle(300, 100, 100);
+
+ //Nude oval
+  fill(227, 188, 154);
+  ellipse(170, 175, 80, 100);
+
+   //Nude oval
+  fill(227, 188, 154);
+  ellipse(230, 175, 80, 100);
+
+    //Nude oval
+  fill(227, 188, 154);
+  ellipse(230, 235, 100, 80);
+
+
+    //Nude oval
+  fill(227, 188, 154);
+  ellipse(170, 235, 100, 80);
+
+  //Nude oval
+  fill(255, 255, 255);
+  ellipse(170, 175, 30, 70);
+
+   //Nude oval
+  fill(255, 255, 255);
+  ellipse(170, 175, 30, 70);
+
+
+
+
+
+
+
+  
+  //black dot--note use of strokeweight to change the size of the point
+  strokeWeight(3);
+  stroke(0);
+  point(550, 550);
 }
